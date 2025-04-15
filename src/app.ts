69f70d0c7cdf12fa863f59787886b09bafeaf890
zwezaynyithegod23 <zwezaynyi@gmail.com>
2025-04-15 19:02:32 +0700

    installation, configuration, and middleware for Express.js

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
+
+export const app = express();
+
+app
+  .use(morgan("dev"))
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(cors());
