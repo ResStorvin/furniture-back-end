7261c80a11a1bc105baccd1d3ce5a4a056520b3b
zwezaynyithegod23 <zwezaynyi@gmail.com>
2025-04-18 21:22:46 +0700

    updating middleware

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import helmet from "helmet";
 import compression from "compression";
 import morgan from "morgan";
 
+import { limiter } from "./middlewares/rateLimiter";
+
 export const app = express();
 
 app
@@ -12,4 +14,11 @@ app
   .use(express.json())
   .use(cors())
   .use(helmet())
-  .use(compression());
+  .use(compression({}))
+  .use(limiter);
+
+// http://localhost:8080/health
+// to test limiters
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
