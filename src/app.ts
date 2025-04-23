13cfcd1d43c4d4a389cbd189d1b2b065728e763d
zwezaynyithegod23 <zwezaynyi@gmail.com>
2025-04-23 23:04:47 +0700

    updating middlware for error handling

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { check } from "./middlewares/check";
 import { CustomRequest } from "./middlewares/check";
 
 import { limiter } from "./middlewares/rateLimiter";
+import { stat } from "fs";
 
 export const app = express();
 
@@ -26,7 +27,7 @@ app.get("/health", check, (req: CustomRequest, res: Response) => {
 
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   const status = error.status || 500;
-  const message = error.message || "Internal Server Error";
-  const errorCode = error.code || "Error_Code";
+  const message = error.message || "internal server error";
+  const errorCode = error.code || "Error Code";
   res.status(status).json({ message, errorCode });
 });
--- src/middlewares/check.ts
+++ src/middlewares/check.ts
@@ -9,6 +9,12 @@ export const check = (
   res: Response,
   next: NextFunction
 ) => {
+  const err: any = new Error("An error has occurred!");
+  err.status = 401;
+  err.code = "Error token expired";
+  return next(err);
+  // have to write "return" when we want to return an error from middleware
+
   req.userId = 333;
   next();
 };
